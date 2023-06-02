5246364f42cef72e0bd8501bcba2f80a4ef4fb06
Gayathri-129 <131529701+Gayathri-129@users.noreply.github.com>
2023-06-02 21:32:12 +0530

    Update task4.py

--- task4.py
+++ task4.py
@@ -3,18 +3,26 @@ class Account:
     def __init__(self,title,balance):
         self.title = title
         self.balance = balance
+    
+    def getBalance(self):
+        return self.balance
+    
+    def deposit(self,amount):
+        self.balance += amount
         
 class SavingsAccount(Account):
 
-    def __init__(self,title,balance,interest_rate):
+    def __init__(self,title,balance,interestRate):
         super().__init__(title, balance)
-        self.interest_rate = interest_rate
+        self.interestRate = interestRate
 
 account = Account("Ashish", 5000)
 print(account.title)
 print(account.balance)
-
+balance = 2000
+account.deposit(500)
+print(account.getBalance())
 savings_account = SavingsAccount("Ashish", 5000, 5)
 print(savings_account.title)
 print(savings_account.balance)
-print(savings_account.interest_rate)
+print(savings_account.interestRate)
